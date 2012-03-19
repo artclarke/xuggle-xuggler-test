8fd44c8ed75040c57581002bdb4e62785d9cd2e3
Art Clarke <art@connectsolutions.com>
2012-03-18 17:45:13 -0700

    initial commit

---
+++ src/main/java/com/xuggle/xuggler/App.java
@@ -0,0 +1,13 @@
+package com.xuggle.xuggler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "The World's Most Useless Jar!" );
+    }
+}
